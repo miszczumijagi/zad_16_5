bdc8690622a54fab8a7ffdea3d955a44048d0e99
Kamil Stangret <kamilstangret@wp.pl>
2018-02-17 20:07:07 +0100

    Add plugins to project and content to TodoList.js and Title.js. Add css styles.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import uuid from 'uuid';
 import style from './App.css';
+import Title from '../components/Title';
+import TodoList from '../components/TodoList';
 
 class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            data: []
+            data: [{
+                id: 1,
+                    text: 'clean room'
+                }, {
+                id: 2,
+                    text: 'wash the dishes'
+                }, {
+                id: 3,
+                    text: 'feed my cat'
+                }]
         };
     }
     addTodo(val){
@@ -24,7 +35,8 @@ class App extends React.Component {
     render() {
         return (
             <div className={style.TodoApp}>
-              <h1>Tutaj pojawią się komponenty naszej aplikacji. </h1> 
+                <Title className={style.TodoTitle} todoCount={this.state.data.length}/>
+                <TodoList className={style.TodoList} data={this.state.data} remove={this.removeTodo.bind(this)}/>
 
       
             </div>
